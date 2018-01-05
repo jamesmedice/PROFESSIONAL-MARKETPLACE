577c2a6f6451e5110f5e9521ab134fae9477b105
JAMES <tiago.sllater@gft.com>
2018-01-05 17:23:26 +0100

    merged swagger

--- MarketPlace-WADL/src/main/java/com/it/gft/global/wadl/config/SwaggerConfigurer.java
+++ MarketPlace-WADL/src/main/java/com/it/gft/global/wadl/config/SwaggerConfigurer.java
@@ -1,6 +1,7 @@
 package com.it.gft.global.wadl.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -14,6 +15,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
  * @author TOSS
  *
  */
+@Configuration
 @EnableSwagger2
 public class SwaggerConfigurer {
 
