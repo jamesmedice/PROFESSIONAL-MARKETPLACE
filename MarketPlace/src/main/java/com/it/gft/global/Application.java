9f4b5ad994b5085e0beb0457f43df2e91c783205
JAMES <tiago.sllater@gft.com>
2018-04-28 13:01:04 +0200

    fix cluster

--- MarketPlace/src/main/java/com/it/gft/global/Application.java
+++ MarketPlace/src/main/java/com/it/gft/global/Application.java
@@ -3,7 +3,9 @@ package com.it.gft.global;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 
 /**
@@ -12,7 +14,9 @@ import org.springframework.context.annotation.ComponentScan;
  *
  */
 @ComponentScan(basePackages = { "com.it.gft.global" })
+@EnableConfigurationProperties
 @SpringBootApplication
+@ConfigurationProperties(value = "classpath:application.yaml")
 public class Application extends SpringBootServletInitializer {
 
     public Application() {
