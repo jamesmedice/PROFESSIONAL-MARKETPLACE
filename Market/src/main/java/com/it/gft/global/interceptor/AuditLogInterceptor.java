c333f83995f6dbc5f56980ba7d3ca41c9eba0e65
Pratis Médice <a73s@gft.com>
2018-08-03 10:17:48 +0200

    sonar qube violation

--- Market/src/main/java/com/it/gft/global/interceptor/AuditLogInterceptor.java
+++ Market/src/main/java/com/it/gft/global/interceptor/AuditLogInterceptor.java
@@ -16,98 +16,98 @@ import com.it.gft.global.interceptor.util.AuditLogUtil;
 import com.it.gft.global.log.model.IAuditLog;
 
 public class AuditLogInterceptor extends EmptyInterceptor {
-    
+
 	private final transient Log LOGGER = LogFactory.getLog(AuditLogInterceptor.class);
 
-    private static final String SAVED = "Saved";
+	private static final String SAVED = "Saved";
 
-    private static final String DELETED = "Deleted";
+	private static final String DELETED = "Deleted";
 
-    private static final String UPDATED = "Updated";
+	private static final String UPDATED = "Updated";
 
-    private static final long serialVersionUID = 1775228354052642640L;
+	private static final long serialVersionUID = 1775228354052642640L;
 
-    private Set<IAuditLog> inserts = new HashSet();
-    private Set<IAuditLog> updates = new HashSet();
-    private Set<IAuditLog> deletes = new HashSet();
+	private Set<IAuditLog> inserts = new HashSet();
+	private Set<IAuditLog> updates = new HashSet();
+	private Set<IAuditLog> deletes = new HashSet();
 
-    Session session;
-    SessionBuilder sessionBuilder;
+	Session session;
+	private transient SessionBuilder sessionBuilder;
 
-    public void setSession(Session session) {
-	this.session = session;
-    }
-
-    public void setSessionBuilder(SessionBuilder sessionBuilder) {
-	this.sessionBuilder = sessionBuilder;
-    }
-
-    @Override
-    public boolean onSave(Object entity, Serializable id, Object[] state, String[] propertyNames, Type[] types) {
-
-	if (entity instanceof IAuditLog) {
-	    AuditLogUtil.LogIt(SAVED, (IAuditLog) entity, session);
-	    inserts.add((IAuditLog) entity);
-	    return false;
-	} else
-	    return super.onSave(entity, id, state, propertyNames, types);
-
-    }
-
-    @Override
-    public void onDelete(Object entity, Serializable id, Object[] state, String[] propertyNames, Type[] types) {
-
-	super.onDelete(entity, id, state, propertyNames, types);
-
-	if (entity instanceof IAuditLog) {
-	    AuditLogUtil.LogIt(DELETED, (IAuditLog) entity, session);
-	    deletes.add((IAuditLog) entity);
+	public void setSession(Session session) {
+		this.session = session;
 	}
-    }
 
-    @Override
-    public void postFlush(Iterator entities) {
-
-	super.postFlush(entities);
-
-	try {
-
-	    for (IAuditLog iAuditLog : inserts) {
-		LOGGER.info("postFlush S [" + iAuditLog.toString() + "]");
-		LOGGER.info("postFlush - insert");
-	    }
-	    for (IAuditLog iAuditLog : deletes) {
-		LOGGER.info("postFlush D [" + iAuditLog.toString() + "]");
-		LOGGER.info("postFlush - delete");
-	    }
-	    for (IAuditLog iAuditLog : updates) {
-		LOGGER.info("postFlush U [" + iAuditLog.toString() + "]");
-		LOGGER.info("postFlush - update");
-	    }
-
-	} finally {
-	    inserts.clear();
-	    updates.clear();
-	    deletes.clear();
+	public void setSessionBuilder(SessionBuilder sessionBuilder) {
+		this.sessionBuilder = sessionBuilder;
 	}
-    }
 
-    @Override
-    public void preFlush(Iterator entities) {
-	super.preFlush(entities);
-    }
+	@Override
+	public boolean onSave(Object entity, Serializable id, Object[] state, String[] propertyNames, Type[] types) {
 
-    @Override
-    public boolean onFlushDirty(Object entity, Serializable id, Object[] currentState, Object[] previousState, String[] propertyNames, Type[] types) {
-	LOGGER.info("onFlushDirty");
+		if (entity instanceof IAuditLog) {
+			AuditLogUtil.LogIt(SAVED, (IAuditLog) entity, session);
+			inserts.add((IAuditLog) entity);
+			return false;
+		} else
+			return super.onSave(entity, id, state, propertyNames, types);
 
-	if (entity instanceof IAuditLog) {
-	    AuditLogUtil.LogIt(UPDATED, (IAuditLog) entity, session);
-	    updates.add((IAuditLog) entity);
-	    return false;
-	} else
-	    return super.onFlushDirty(entity, id, currentState, previousState, propertyNames, types);
+	}
 
-    }
+	@Override
+	public void onDelete(Object entity, Serializable id, Object[] state, String[] propertyNames, Type[] types) {
+
+		super.onDelete(entity, id, state, propertyNames, types);
+
+		if (entity instanceof IAuditLog) {
+			AuditLogUtil.LogIt(DELETED, (IAuditLog) entity, session);
+			deletes.add((IAuditLog) entity);
+		}
+	}
+
+	@Override
+	public void postFlush(Iterator entities) {
+
+		super.postFlush(entities);
+
+		try {
+
+			for (IAuditLog iAuditLog : inserts) {
+				LOGGER.info("postFlush S [" + iAuditLog.toString() + "]");
+				LOGGER.info("postFlush - insert");
+			}
+			for (IAuditLog iAuditLog : deletes) {
+				LOGGER.info("postFlush D [" + iAuditLog.toString() + "]");
+				LOGGER.info("postFlush - delete");
+			}
+			for (IAuditLog iAuditLog : updates) {
+				LOGGER.info("postFlush U [" + iAuditLog.toString() + "]");
+				LOGGER.info("postFlush - update");
+			}
+
+		} finally {
+			inserts.clear();
+			updates.clear();
+			deletes.clear();
+		}
+	}
+
+	@Override
+	public void preFlush(Iterator entities) {
+		super.preFlush(entities);
+	}
+
+	@Override
+	public boolean onFlushDirty(Object entity, Serializable id, Object[] currentState, Object[] previousState, String[] propertyNames, Type[] types) {
+		LOGGER.info("onFlushDirty");
+
+		if (entity instanceof IAuditLog) {
+			AuditLogUtil.LogIt(UPDATED, (IAuditLog) entity, session);
+			updates.add((IAuditLog) entity);
+			return false;
+		} else
+			return super.onFlushDirty(entity, id, currentState, previousState, propertyNames, types);
+
+	}
 
 }
