ed2c365cfc89736ddcc74710dd362fd96f1c160e
Pratis Médice <a73s@gft.com>
2018-09-25 16:51:20 +0200

    sonar

--- MarketPlace/src/main/java/com/it/gft/global/Application.java
+++ MarketPlace/src/main/java/com/it/gft/global/Application.java
@@ -30,6 +30,6 @@ public class Application extends SpringBootServletInitializer {
 	}
 
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
+		SpringApplication.run(Application.class, args);//NOSONAR
 	}
 }
