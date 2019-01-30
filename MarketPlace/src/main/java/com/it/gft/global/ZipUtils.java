200ee3d2ccc9fcf4d61a0cf4b0952469425be329
Pratis Médice <a73s@gft.com>
2019-01-30 15:31:14 +0100

    ziputils

--- MarketPlace/src/main/java/com/it/gft/global/ZipUtils.java
+++ MarketPlace/src/main/java/com/it/gft/global/ZipUtils.java
@@ -11,10 +11,9 @@ import org.springframework.util.StringUtils;
 import javax.servlet.http.HttpServletResponse;
 
 public class ZipUtils {
-	
 
 	public static final String APPLICATION_ZIP = "application/zip";
-		
+
 	public static void setZipResponse(HttpServletResponse response, byte[] bytesFileZip, ZipEntry zipEntry, String fileName) throws Exception {
 		if (bytesFileZip == null || bytesFileZip.length == 0)
 			throw new Exception("The parameter bytesFileZip is empty");
@@ -25,19 +24,22 @@ public class ZipUtils {
 		response.setHeader("Content-Disposition", "attachment; filename=" + fileName);
 		response.setContentType(APPLICATION_ZIP);
 
+		ZipOutputStream zippedOut = null;
+		
 		try {
-			ZipOutputStream zippedOut = new ZipOutputStream(response.getOutputStream()); // NOSONAR
+			zippedOut = new ZipOutputStream(response.getOutputStream());
 			zippedOut.putNextEntry(zipEntry);
 			InputStream is = new ByteArrayInputStream(bytesFileZip);
 			StreamUtils.copy(is, zippedOut);
 
-			zippedOut.finish();
-			zippedOut.flush();
 			response.getOutputStream().flush();
 
 		} catch (Exception x) {
 			throw x;
 		} finally {
+			zippedOut.finish();
+			zippedOut.flush();
+
 			response.getOutputStream().close();
 		}
 	}
