668766848a685a188c818af8401310f0d2d560ec
Pratis Médice <a73s@gft.com>
2019-02-01 10:44:39 +0100

    boot app

--- MarketPlace/src/main/java/com/it/gft/global/Application.java
+++ MarketPlace/src/main/java/com/it/gft/global/Application.java
@@ -11,7 +11,7 @@ import org.springframework.context.annotation.ComponentScan;
 /**
  * 
  * @author a73s
- * @author TIAGO MEDICI
+ * @author TIAGO P MEDICI
  *
  */
 @ComponentScan(basePackages = { "com.it.gft.global" })
